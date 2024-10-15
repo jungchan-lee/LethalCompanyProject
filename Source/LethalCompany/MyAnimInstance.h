185e7b94b53c10e039be1febf4c1d13f1a737e7a
HyunJun Sung <shj9866@gmail.com>
2024-10-15 14:06:39 +0900

    Add animinstance

---
+++ Source/LethalCompany/MyAnimInstance.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "MyAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LETHALCOMPANY_API UMyAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	FVector Velocity;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	float GroundSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	uint8 bShouldMove : 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	uint8 bIsFalling : 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	float Direction;
+};
