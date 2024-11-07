0d540eaacdff98ae5cf85bce555746acd69e07e4
HyunJun Sung <shj9866@gmail.com>
2024-11-07 17:47:23 +0900

    Add Lobby GameState,Widget

---
+++ Source/LethalCompany/LobbyGameStateBase.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "LobbyGameStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LETHALCOMPANY_API ALobbyGameStateBase : public AGameStateBase
+{
+	GENERATED_BODY()
+
+
+};
